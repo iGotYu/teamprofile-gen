fc1e7d88b7e56049e177911049dc7b3aadc47c58
Andrew Yu <igotyuu421@gmail.com>
2021-04-15 22:16:22 -0700

    initial commit

---
+++ Develop/lib/Engineer.js
@@ -0,0 +1,6 @@
+// TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require("../lib/Employee");
+
+class Engineer extends Employee {
+    constructor
+}
---
+++ Develop/lib/Manager.js
@@ -0,0 +1,2 @@
+// TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+const Employee = require("../lib/Employee");
